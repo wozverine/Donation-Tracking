bea6a91cda1beb45ad357198ffbb7a58b00e9f2b
Serra Gengeç <63043941+serrabersan@users.noreply.github.com>
2020-04-23 22:58:14 +0300

    Update main.java

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class main {
 
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
+		// Buradaki kod personal info sayfasını açacak action içine gidecek
 		personal p = new personal();
 		//p.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		p.setSize(700, 600);
